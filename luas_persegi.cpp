a8d2de829f3aba5b49856b3dc4dd866524ae1697
adityaSI1 <322510001@student.machung.ac.id>
2025-11-04 17:27:13 +0700

    menambahkan luas persegi

---
+++ luas_persegi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float sisi, luas;
+
+    cout << "Masukkan panjang sisi persegi: ";
+    cin >> sisi;
+
+    luas = sisi * sisi;
+
+    cout << "Luas persegi adalah: " << luas << endl;
+
+    return 0;
+}
